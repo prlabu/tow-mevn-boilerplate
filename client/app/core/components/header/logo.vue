e41c0dc372b6e40888e39e9d04d992517bb37868
prlabu <1thisisfornothing1@gmail.com>
2020-03-27 16:54:36 -0500

    initial commit, running locally

---
+++ client/app/core/components/header/logo.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+	.logo
+		a.nav-item(href="#")
+			span 
+				strong TUG
+				| oWrd
+</template>
+
+<script>
+
+	export default {
+
+	};
+	
+</script>
+
+<style lang="scss">
+</style>
